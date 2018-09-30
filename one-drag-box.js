256b8a3eb028b6ea8149e2aaab9c62bb80a9f06c
Alex Martin <alexmartinturrillas@gmail.com>
2018-09-30 14:06:07 +0200

    script update

--- one-drag-box.js
+++ one-drag-box.js
@@ -1,201 +1,102 @@
 import {OneClass, html} from '@alexmtur/one-class'
 import {oneStyle} from '@alexmtur/one-style'
+//import * as smoothscroll from 'smoothscroll-polyfill';
 
 export class OneDragBox extends OneClass {
     static get properties() {return {
-        hour: {type: Number, public: true},     //0 - 23 
-        minute: {type: Number, public: true},   //0 - 59 
-        hourString: {type: String, public: true},     //0 - 23 
-        minuteString: {type: String, public: true},   //0 - 59 
-        amPm: {type: String, public: true},     //am - pm
+        draggable: Boolean, 
+        column: Boolean,
+        slectedIndex: Number,
     }}
     constructor() {
         super();  
+        this.selectedIndex = 0;
+        //smoothscroll.polyfill();
+        //console.log(smoothscroll);
     }
     _firstRendered() {
         super._firstRendered();
-        
-        //this.id('clockPage' + this.amPm).scrollIntoView();
+        let index = 0;
+        this.id('content').assignedNodes().map((child) => {
+            if(child.nodeName !== '#text') {
+                child.setAttribute("id", "page" + index);
+                child.setAttribute("class", "page");
+                this.id('dragBox').appendChild(child);
+                index += 1;
+            }
+        });
+        this.childrenNumber = index;
+        this.id('dragBox').removeChild(this.id('content'));
+        this.id('page' + this.selectedIndex).scrollIntoView();
+        //smoothscroll.polyfill();
+        //add touchevents for safari and touchscreens
+        //add polyfill for safari scroll smooth;
+
+    }
+    next() {
+        if(this.selectedIndex < this.childrenNumber - 1)  this.selectedIndex += 1;
+        this.id('page' + this.selectedIndex).scrollIntoView({behavior: 'smooth'}); 
+    }
+    previous() {
+        if(this.selectedIndex > 0) this.selectedIndex -= 1;
+        this.id('page' + this.selectedIndex).scrollIntoView({behavior: 'smooth'}); 
     }
     setupDrag(e) {
         e.preventDefault();
         this.mousedown = true;
         this.initialX = e.pageX;
-        this.initialScrollX = this.id('hourPanel').scrollLeft;
+        this.initialScrollX = this.id('dragBox').scrollLeft;
         this.scrollDelta = 0;
+        //console.log('a');
         return false;
+
     }
     dragPage(e) {
         if(!this.mousedown) return;
         e.preventDefault();
         this.scrollDelta = this.initialX - e.pageX;
-        this.id('hourPanel').scrollTo((this.scrollDelta + this.initialScrollX), 0);
+        this.id('dragBox').scrollTo((this.scrollDelta + this.initialScrollX), 0);
+        //console.log('b')
     }
     updatePage(e) {
         e.preventDefault();
         if(this.mousedown) {
-            let width = this.id('hourPanel').offsetWidth;
-            if(this.scrollDelta > width / 2.5 && this.amPm === 'am') {
-                this.selectAmPm('pm');
-            }
-            else if((-1) * this.scrollDelta > width / 2.5 && this.amPm === 'pm') { 
-                this.selectAmPm('am');
-            }
-            else if(Math.abs(this.scrollDelta) > 0) 
-                this.id('clockPage' + this.amPm).scrollIntoView({behavior: 'smooth'});           
+            let width = this.id('dragBox').offsetWidth;
+            if(this.scrollDelta > width / 2.5) this.next();
+            else if((-1) * this.scrollDelta > width / 2.5) this.previous();
+            else if(Math.abs(this.scrollDelta) > 0)  this.id('page' + this.selectedIndex).scrollIntoView({behavior: 'smooth'});                       
             this.mousedown = false;
         }
+        //console.log('c')
     } 
-
      _render() {
         return html`
         ${oneStyle}
         <style>
-            /* local DOM styles go here */
             :host {
                 display: block;
-                color: #333;              
             }
-            #digitalText {
-                font-family: Digital !important;
-                font-size: 430%;
-            }
-            #clock {
+            #dragBox {
                 display: flex;  
-                flex-direction: column;              
-                width: 100%; 
-                max-width: 500px;
-                min-width: 200px;
-                flex: initial;
-                flex-shrink: 1;
-                flex-grow: 0;
-            }
-            #clockHeader {
-                display: flex;  
-                align-items: center;
-                justify-content: space-between;              
-            }
-            #clockBody {
-                position: relative;
-                width: 100%; 
-                display: flex; 
-            }
-            #hourPanel, #minutePanel {
+                flex-direction: row;              
                 width: 100%; 
                 overflow-x: hidden;
-                display: flex; 
-                box-shadow: 0 2px 7px rgba(0, 0, 0, 0.25);
             }
-            #amPm {
-                box-shadow: 0 2px 7px rgba(0, 0, 0, 0.25);
-                margin-bottom: 10px;
-                display: flex;
-                max-width: 150px;              
-            }
-            .amPmButton {
-                font-size: 120%;
-                cursor: pointer;
-                padding: 10px;
-                color: #bbb;
-                flex: 1;
-               
-            }
-            .amPmButton[selected=true] {
-                color: white;
-                background: var(--one-color, #333);
-            }
-            .blink {
-                animation: blink 1s step-start 0s infinite;
-            }
-            @keyframes blink {
-              50% {
-                opacity: 0;
-              }
-            }
-            .clockPage { 
-                min-width: 100%;  
-                display: flex;                 
-                z-index: 0;
-                flex-wrap: wrap;
-                justify-content: center;
+            .page {
+                min-width: 100%;
+                max-width: 100%; 
+                display: flex;  
                 align-items: center;
+                justify-dragBox: center;
             }
-            .cell {
-                width:25%;
-                height: 8vh;
-                -webkit-transition: background .5s ease;
-                -moz-transition: background .5s ease;
-                border-radius: 100%;
-                cursor: pointer;
-                display: flex;                 
-                justify-content: center;
-                align-items: center;
-            }            
-            .cell[selected=true] .time, .time:hover {
-                background: var(--one-color, #333);
-                color: white;
-            }  
-            .cell:hover {
-                opacity: 0.5;                
-            } 
-            .cell:active {
-                opacity: 1
-            }                     
-            .cell[currentMonth=false] .time {
-                color: #bbb;
-            }
-            .time {
-                width: 6vh;
-                height: 6vh;
-                border-radius: 100%;
-                display: flex;
-                justify-content: center;
-                align-items: center;
-            }
-            #dots {
-                display: flex; 
-                align-items: center; 
-                justify-content: center; 
-                flex-direction: column;
-            }
-            .dot {
-                height: 8px; 
-                width: 8px; 
-                background: var(--one-color, #333); 
-                margin:10px; 
-                border-radius: 0%;
-            }
-            #hourText {
-                height: 100%;
-                width: 100%;
-                
-                position: absolute;
-                font-size: 20vh;
-                font-family: Digital !important;
-                font-weight: normal;
-                font-style: normal;
-                opacity: 0.3;
-                padding: 0;
-                display: flex;
-                justify-content: center;
-                align-items: center;
-                transform: scaleX(0.5);
-            }
-
         </style>
 
-        <div id="clock"> 
-            <div id="clockBody">              
-                <div id="hourPanel" 
-                    @mousedown=${(e)=>{this.setupDrag(e)}}    
-                    @mousemove=${(e)=>{this.dragPage(e)}}
-                    @mouseup=${(e)=>{this.updatePage(e)}}>
-                    <slot></slot>           
-                </div> 
-            </div>   
+        <div id="dragBox" 
+                    @pointerdown=${(e)=>{this.setupDrag(e)}}
+                    @pointermove=${(e)=>{this.dragPage(e)}}
+                    @pointerup=${(e)=>{this.updatePage(e)}}> 
+            <slot id="content" @mousedown=${(e)=>{this.setupDrag(e)}} ></slot>  
         </div>
         `;}
 }
 customElements.define('one-drag-box', OneDragBox);
-
-
